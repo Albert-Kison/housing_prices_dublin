e72f118b67c49b6214142bfd4ad0e0e7a5a405ed
Albert Kison <albert.kison@student.griffith.ie>
2022-09-17 04:50:28 +0100

    Add comments

--- main.py
+++ main.py
@@ -1,69 +1,53 @@
-import requests
+import os
 import time
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from geopy.geocoders import GoogleV3
 
-chromedriver_path = "/Users/albertkison/Desktop/chromedriver"
+chromedriver_path = os.getenv("CHROMEDRIVER_PATH")
 driver = webdriver.Chrome(executable_path=chromedriver_path)
 
 data = []
 
 driver.get("https://www.daft.ie/property-for-rent/dublin?showMap=false&sort=publishDateDesc&pageSize=20&from=0")
-time.sleep(3)
-driver.find_elements(By.CSS_SELECTOR, ".cc-modal__main .cc-modal__btn")[1].click()
+time.sleep(3)   # some time to load all the elements
+driver.find_elements(By.CSS_SELECTOR, ".cc-modal__main .cc-modal__btn")[1].click()  # accept cookies
 time.sleep(3)
 page = 0
+# going through 13 pages
 while page < 14:
-    time.sleep(2)
+    time.sleep(2)   # giving some time to load when going to the next page
     page += 1
-    print(page)
-    results = driver.find_elements(By.CSS_SELECTOR, ".djuMQD")
-    print(len(results))
-    for result in results:
+    # print(page)   DEBUG
+    page_results = driver.find_elements(By.CSS_SELECTOR, ".djuMQD")
+    # print(len(results))   DEBUG
+    for result in page_results:
         link = result.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
-        print(link)
+
         price = ""
+        # check if the element exists
         if len(result.find_elements(By.CSS_SELECTOR, ".hiFkJc span")) > 0:
             price = result.find_element(By.CSS_SELECTOR, ".hiFkJc span").text
         print(price)
+
         address = ""
+        # check if the element exists
         if len(result.find_elements(By.CSS_SELECTOR, ".dzihyY")) > 0:
             address = result.find_element(By.CSS_SELECTOR, ".dzihyY").text
         print(address)
-        object = {
+
+        page_data = {
             "link": link,
             "price": price,
             "address": address,
         }
-        data.append(object)
+        data.append(page_data)
+
+    # go to the next page
     driver.find_elements(By.CSS_SELECTOR, ".dPixQH span")[-1].click()
-# for i in range(0, 4):
-#     print("rogjrg")
-#     time.sleep(5)
-#     if len(driver.find_elements(By.CSS_SELECTOR, "#mh-modal-body .MhIcon")) > 0:
-#         driver.find_element(By.CSS_SELECTOR, "#mh-modal-body .MhIcon").click()
-#     results = driver.find_elements(By.CSS_SELECTOR, ".PropertyListingCard")
-#     print(len(results))
-#     for result in results:
-#         link = result.find_element(By.CSS_SELECTOR, ".PropertyListingCard__ImageContainer a").get_attribute("href")
-#         print(link)
-#         price = result.find_element(By.CSS_SELECTOR, ".PropertyListingCard__Price div").text
-#         print(price)
-#         address = result.find_element(By.CSS_SELECTOR, ".PropertyListingCard__Address").text
-#         print(address)
-#         object = {
-#             "link": link,
-#             "price": price,
-#             "address": address,
-#         }
-#         data.append(object)
-#     time.sleep(3)
-#     if i < 3:
-#         driver.find_element(By.CSS_SELECTOR, ".ngx-pagination .pagination-next").click()
-#
-# print(len(data))
+
+
+# importing data to excel
 df = pd.DataFrame(data)
 print(df)
 df.to_excel("data.xlsx")
